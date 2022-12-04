ed09954bc3a211955cd6de68122a538102285247
adarshaadhungel1 <dadarsha6@gmail.com>
2022-12-04 13:38:11 +0545

    added a function to get the values based on the buttons clicked

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+function getInnerValue(e){
+    let keyValue = e.target.innerHTML;
+}
+
 let btnzero = document.querySelector("#zero");
 let btnone = document.querySelector("#one");
 let btntwo = document.querySelector("#two");
@@ -13,21 +17,21 @@ let btnadd = document.querySelector("#plus");
 let btnsub = document.querySelector("#minus");
 let btnmult = document.querySelector("#multiply");
 let btndiv = document.querySelector("#divide");
-let btnequal = document.querySelector("#equal");
+let btnequal = document.querySelector("#equals");
 
-btnzero.addEventListener('click',);
-btnone.addEventListener('click',);
-btntwo.addEventListener('click',);
-btnthree.addEventListener('click',);
-btnfour.addEventListener('click',);
-btnfive.addEventListener('click',);
-btnsix.addEventListener('click',);
-btnseven.addEventListener('click',);
-btneight.addEventListener('click',);
-btnnine.addEventListener('click',);
-btndecimal.addEventListener('click',);
-btnadd.addEventListener('click',);
-btnsub.addEventListener('click',);
-btnmult.addEventListener('click',);
-btndiv.addEventListener('click',);
-btnequal.addEventListener('click',);
+btnzero.addEventListener('click',getInnerValue);
+btnone.addEventListener('click',getInnerValue);
+btntwo.addEventListener('click',getInnerValue);
+btnthree.addEventListener('click',getInnerValue);
+btnfour.addEventListener('click',getInnerValue);
+btnfive.addEventListener('click',getInnerValue);
+btnsix.addEventListener('click',getInnerValue);
+btnseven.addEventListener('click',getInnerValue);
+btneight.addEventListener('click',getInnerValue);
+btnnine.addEventListener('click',getInnerValue);
+btndecimal.addEventListener('click',getInnerValue);
+btnadd.addEventListener('click',getInnerValue);
+btnsub.addEventListener('click',getInnerValue);
+btnmult.addEventListener('click',getInnerValue);
+btndiv.addEventListener('click',getInnerValue);
+btnequal.addEventListener('click',getInnerValue);
